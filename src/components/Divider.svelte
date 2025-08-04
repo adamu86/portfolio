313b276b5bbdd07747065bb4f08a648e05dc4104
Adam Grzeszczuk <adameq02@outlook.com>
2025-08-04 21:10:12 +0200

    -

---
+++ src/components/Divider.svelte
@@ -0,0 +1,10 @@
+<script lang=ts>
+    let { direction = 'vertical' } = $props();
+
+    const directionClasses: Record<string, string> = {
+        vertical: 'h-full w-px mx-auto',
+        horizontal: 'w-full h-px my-auto'
+    }
+</script>
+
+<div class="{directionClasses[direction]} bg-white/15 dark:bg-black/15"></div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,62 @@
+<script>
+
+</script>
+
+<main class="min-w-120 max-w-200 mx-auto p-6 grid gap-6">
+    <nav class="grid place-items-center gap-4">
+        <div class="bg-white/10 w-56 h-56 rounded-full"></div>
+        <h1 class="text-4xl">
+            Adam Grzeszczuk
+        </h1>
+    </nav>    
+    <ul class="grid gap-12 bg-black/10 p-6 rounded-sm">
+        <li class="flex flex-col gap-4">
+            <h2 class="text-3xl">
+                O mnie
+            </h2>
+            
+            <div>
+                <h3 class="text-2xl">
+                    Wykształcenie
+                </h3>
+                <p>
+                    Ukończyłem studia inżynierskie na Politechnice Białostockiej w 2025 roku z wynikiem bardzo dobrym. Obecnie kontynuuję naukę na studiach magisterskich na tej samej uczelni. 
+                </p>
+            </div>
+
+            <div>
+                <h3 class="text-2xl">
+                    Zainteresowania
+                </h3>
+            </div>
+
+            <div class="">
+                <h2 class="text-3xl">
+                    Umiejętności
+                </h2>
+                
+            </div>
+        </li>
+
+        <li class="flex flex-col gap-4">
+            <h2 class="text-3xl">
+                Zainteresowania
+            </h2>
+            <div>
+                
+            </div>
+        </li>
+
+        <li>
+            <h2 class="text-3xl">
+                Projekty
+            </h2>
+        </li>
+
+        <li class="flex flex-col gap-4">
+            <h2 class="text-3xl">
+                Kontakt
+            </h2>
+        </li>
+    </ul>
+</main>
